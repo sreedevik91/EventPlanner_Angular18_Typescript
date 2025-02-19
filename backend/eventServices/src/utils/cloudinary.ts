2d0fc7c1db9df756b6739f1c112a1e2af8f1ba30
Sreedevi <sreedevikpadmakumari@gmail.com>
2025-02-19 12:15:01 +0530

    removed confirm and added bootstrap model for confirm box, added options to set url secured for cloudinary uploads

--- backend/eventServices/src/utils/cloudinary.ts
+++ backend/eventServices/src/utils/cloudinary.ts
@@ -1,4 +1,4 @@
-import {v2 as cloudinary} from 'cloudinary'
+import {v2 as cloudinary, UploadApiOptions} from 'cloudinary'
 import { config } from 'dotenv'
 
 config()
@@ -9,4 +9,14 @@ cloudinary.config({
     api_secret:process.env.CLOUDINARY_API_SECRET
 })
 
-export default cloudinary
+// export default cloudinary
+
+export const getImgUrl = async (img: string, options?: UploadApiOptions) => {
+    try {
+        const uploadResult = await cloudinary.uploader.upload(img, options)
+        console.log('cloudinary img upload result: ', uploadResult);
+        return { success: true, data: { imgUrl: uploadResult?.url, type: uploadResult.resource_type } }
+    } catch (error) {
+        return { success: false, message: 'Image not saved.' }
+    }
+}
