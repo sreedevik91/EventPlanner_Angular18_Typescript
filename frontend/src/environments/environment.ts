d6b775d569ad15e48cf12cedc8af22c7ead2161d
Sreedevi <sreedevikpadmakumari@gmail.com>
2025-03-09 20:58:34 +0530

    updated nginx confog angular environment variables and dockerfiles to run docker containers successfully

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+    production: true,
+    apiUserUrl: 'http://api-gateway:4000/api/user/', // Use Docker service names
+    apiServiceUrl: 'http://api-gateway:4000/api/service/',
+    apiEventUrl: 'http://api-gateway:4000/api/event/',
+    apiBookingUrl: 'http://api-gateway:4000/api/booking/',
+    apiChatUrl: 'http://api-gateway:4000/api/chat/',
+    serviceImgUrl: 'http://api-gateway:4000/api/service/uploads/',
+    eventImgUrl: 'http://api-gateway:4000/api/event/uploads/',
+    socketBackendUrl: 'http://api-gateway:4000',
+    razorpayKeyId: 'rzp_test_85xo26IPuPICSY'
+};
