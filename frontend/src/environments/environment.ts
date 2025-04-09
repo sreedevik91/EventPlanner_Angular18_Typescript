0ae4567f580a3460f4547edeaf1993f41920e915
Sreedevi <sreedevikpadmakumari@gmail.com>
2025-04-10 01:59:09 +0530

    testing for httpOnly cookies

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -15,28 +15,28 @@
 //   razorpayKeyId: 'rzp_test_85xo26IPuPICSY'
 // };
 
-// export const environment = {
-//   production: true,
-//   apiUserUrl: '/api/user/',
-//   apiServiceUrl: '/api/service/',
-//   apiEventUrl: '/api/event/',
-//   apiBookingUrl: '/api/booking/',
-//   apiChatUrl: '/api/chat/',
-//   apiWalletUrl: '/api/wallet/',
-//   socketBackendUrl: '/', // Relative path for WebSocket
-//   googleAuthUrl: '/api/user/auth/google',
-//   razorpayKeyId: 'rzp_live_<your_live_key>'
-// };
-
 export const environment = {
   production: true,
-  apiUserUrl: 'https://dreamevents.shop/api/user/',
-  apiServiceUrl: 'https://dreamevents.shop/api/service/',
-  apiEventUrl: 'https://dreamevents.shop/api/event/',
-  apiBookingUrl: 'https://dreamevents.shop/api/booking/',
-  apiChatUrl: 'https://dreamevents.shop/api/chat/',
-  apiWalletUrl: 'https://dreamevents.shop/api/wallet/',
-  socketBackendUrl: 'https://dreamevents.shop/', // Relative path for WebSocket
-  googleAuthUrl: 'https://dreamevents.shop/api/user/auth/google',
+  apiUserUrl: '/api/user/',
+  apiServiceUrl: '/api/service/',
+  apiEventUrl: '/api/event/',
+  apiBookingUrl: '/api/booking/',
+  apiChatUrl: '/api/chat/',
+  apiWalletUrl: '/api/wallet/',
+  socketBackendUrl: '/', // Relative path for WebSocket
+  googleAuthUrl: '/api/user/auth/google',
   razorpayKeyId: 'rzp_live_<your_live_key>'
 };
+
+// export const environment = {
+//   production: true,
+//   apiUserUrl: 'https://dreamevents.shop/api/user/',
+//   apiServiceUrl: 'https://dreamevents.shop/api/service/',
+//   apiEventUrl: 'https://dreamevents.shop/api/event/',
+//   apiBookingUrl: 'https://dreamevents.shop/api/booking/',
+//   apiChatUrl: 'https://dreamevents.shop/api/chat/',
+//   apiWalletUrl: 'https://dreamevents.shop/api/wallet/',
+//   socketBackendUrl: 'https://dreamevents.shop/', // Relative path for WebSocket
+//   googleAuthUrl: 'https://dreamevents.shop/api/user/auth/google',
+//   razorpayKeyId: 'rzp_live_<your_live_key>'
+// };
