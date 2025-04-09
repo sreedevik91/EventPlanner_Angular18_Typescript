ab157d7665af9ef229b428b6120bbbd3871c81d7
Sreedevi <sreedevikpadmakumari@gmail.com>
2025-04-10 00:26:52 +0530

    testing for httpOnly cookies

--- backend/user/dist/redis/redisClient.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const redis_1 = require("redis");
-const redisClient = (0, redis_1.createClient)({ url: 'redis://localhost:6379' });
-redisClient.connect()
-    .then(() => console.log('redis connected'))
-    .catch(console.error);
-exports.default = redisClient;
