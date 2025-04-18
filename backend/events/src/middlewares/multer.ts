878e505c8e9879da04408b4d99140126d68fcfbf
Sreedevi <sreedevikpadmakumari@gmail.com>
2025-04-19 01:17:15 +0530

    multer storage path changed with process.cwd() to ensure consistant path

--- backend/events/src/middlewares/multer.ts
+++ backend/events/src/middlewares/multer.ts
@@ -3,9 +3,12 @@ import path from "path";
 
 const Storage=multer.diskStorage({
     destination:function(req,file,cb){
-        console.log('imagePath from service routes.ts:', path.join(__dirname, '../public'));
+        // console.log('imagePath from service routes.ts:', path.join(__dirname, '../public'));
     
-        cb(null,path.join(__dirname,'../public'))
+        // cb(null,path.join(__dirname,'../public'))
+
+        console.log('imagePath from chat service routes.ts:', path.join(process.cwd(), '/src/public'));
+        cb(null,path.join(process.cwd(), '/src/public'))
     },
     filename:function(req,file,cb){
     
